b97bbe4b153d9bf0aa820a7bff7c57ebfe2f62d2
nalmic <michal.nalewajko.lh@gmail.com>
2022-02-05 17:29:01 +0100

    reactive code from spring.io to demo

--- src/main/java/com/nalmic/reactive/ReactiveApplication.java
+++ src/main/java/com/nalmic/reactive/ReactiveApplication.java
@@ -2,12 +2,15 @@ package com.nalmic.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ReactiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactiveApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ReactiveApplication.class, args);
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		// We need to block for the content here or the JVM might exit before the message is logged
+		System.out.println(">> message = " + greetingClient.getMessage().block());
 	}
-
 }
